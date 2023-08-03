f367e5f9f7f60f1e1c6cdac1813c7778c75ba94b
Dinoman44 <66869201+Dinoman44@users.noreply.github.com>
2023-08-03 20:25:44 +0800

    Remove outdated files

--- unic-github-uploader/config.py
+++
@@ -1,9 +0,0 @@
-from tempfile import mkdtemp
-
-
-GOOGLE_CLIENT_ID = "560689788667-2or0hcftq62kvr8qhik62pjl0lhlatqi.apps.googleusercontent.com"
-GOOGLE_CLIENT_SECRET = "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"
-SESSION_FILE_DIR = mkdtemp()
-SESSION_PERMANENT = False
-SESSION_TYPE = "filesystem"
-TEMPLATES_AUTO_RELOAD = True
