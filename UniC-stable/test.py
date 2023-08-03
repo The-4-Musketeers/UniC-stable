df2c734d55064ebac5a23a88f7eb77dc4a13f518
Dinoman44 <66869201+Dinoman44@users.noreply.github.com>
2023-08-03 22:22:57 +0800

    Delete test.py

--- UniC-stable/test.py
+++ UniC-stable/test.py
@@ -1,17 +1,1 @@
-import mpu
-
-latitude1 = 1.3488607
-longitude1 = 103.96224869999999
-
-latitude2 = 1.3553352
-longitude2 = 103.9482855
-
-
-# latitude is y
-# longitude is x 
-
-
-dist = mpu.haversine_distance((latitude1, longitude1), (latitude2, longitude2))
-
-print(dist)
 
